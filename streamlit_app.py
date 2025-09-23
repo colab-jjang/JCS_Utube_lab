7952c95c6accbecbf10b69c33b1d827a24c5862b
colab-jjang <iyhee8@gmail.com>
2025-09-23 18:08:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ from datetime import timedelta
 import json
 
 # 페이지 설정
-#st.set_page_config(
-#    page_title="YouTube Shorts 랭킹 분석기",
-#    page_icon="📱",
-#    layout="wide",
-#    initial_sidebar_state="expanded"
-#)
+st.set_page_config(
+    page_title="YouTube Shorts 랭킹 분석기",
+    page_icon="📱",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 SHEET_KEY = st.secrets["SHEET_KEY"] 
 SHEET_NAME = "Sheet1"              # 실제 시트 이름
@@ -34,17 +34,17 @@ if "channels_df" not in st.session_state or st.session_state.channels_df is None
     channels_df = DataProcessor.load_df_from_gsheet(ws)
 
     #유효한 url인지 확인
-    st.write(channels_df["channel_url"].head(10))
+#    st.write(channels_df["channel_url"].head(10))
     
-    if (
-        channels_df is not None
-        and not channels_df.empty
-        and 'channel_name' in channels_df.columns
-    ):
-        st.session_state.channels_df = channels_df
-    else:
-        st.session_state.channels_df = pd.DataFrame(
-            columns=['channel_id', 'channel_name', 'channel_url'])
+#    if (
+#        channels_df is not None
+#        and not channels_df.empty
+#        and 'channel_name' in channels_df.columns
+#    ):
+#        st.session_state.channels_df = channels_df
+#    else:
+#        st.session_state.channels_df = pd.DataFrame(
+#            columns=['channel_id', 'channel_name', 'channel_url'])
 
 # CSS 스타일링 (파스텔 테마)
 st.markdown("""
@@ -511,6 +511,7 @@ def display_results(df, source_name):
 
 if __name__ == "__main__":
     main()
+
 
 
 
