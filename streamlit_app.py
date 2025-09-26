34e958799fac0445ef1ced7a24d24d4b96049946
colab-jjang <iyhee8@gmail.com>
2025-09-26 14:06:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -192,13 +192,6 @@ def keyword_search_page(hours, max_results):
             except Exception as e:
                 st.error(f"오류: {str(e)}")
     
-    # ----------- 여기서 랭킹 테이블 바로 그림 -----------
-#    st.subheader("🏆 Shorts 랭킹")
-    
-#    display_df = df[['thumbnail','title', 'channel', 'formatted_views', 'formatted_likes','published_at', 'video_url']].copy()
-#    display_df.columns = ['썸네일','제목', '채널명', '조회수', '좋아요', '발행일', 'LINK']
-    #                display_df.insert(0, '순위', range(1, len(display_df) + 1))
-
     if 'shorts_display_df' in st.session_state:
         st.subheader("🏆 Shorts 랭킹")
         display_df = st.session_state['shorts_display_df']
@@ -574,6 +567,7 @@ def display_results(df, source_name):
 
 if __name__ == "__main__":
     main()
+
 
 
 
